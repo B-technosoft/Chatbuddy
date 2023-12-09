28a2145da2cf4d9d3204a1a36a0a6a34b3e2cbd9
Bhavin Pathak <bhavin.rndtechnosoft@gmail.com>
2023-12-09 10:03:37 +0530

    Initial Commit

---
+++ Chatbuddy/ChatbuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatbuddyApp.swift
+//  Chatbuddy
+//
+//  Created by Ravi Shah on 09/12/23.
+//
+
+import SwiftUI
+
+@main
+struct ChatbuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Chatbuddy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Chatbuddy
+//
+//  Created by Ravi Shah on 09/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
